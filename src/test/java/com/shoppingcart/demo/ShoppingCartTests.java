be112ffa42b9e24aabf85e0ec4035ff4827300f5
Zhiyu517 <wangzhiyu0517@gmail.com>
2020-11-19 23:38:36 -0800

    Shopping Cart 1

---
+++ src/test/java/com/shoppingcart/demo/ShoppingCartTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
