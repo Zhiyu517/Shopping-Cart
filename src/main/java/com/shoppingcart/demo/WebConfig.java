f698c1eb9a22d3dfcf9716ad02b3b8ef23b4be50
Zhiyu517 <wangzhiyu0517@gmail.com>
2020-12-08 18:11:39 -0800

    heroku verision

--- src/main/java/com/shoppingcart/demo/WebConfig.java
+++ src/main/java/com/shoppingcart/demo/WebConfig.java
@@ -2,6 +2,8 @@ package com.shoppingcart.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,21 +13,16 @@ public class WebConfig implements WebMvcConfigurer{
 
     private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
-//
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("home");
-//    }
 
-
-//
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String prefix =  WebConfig.class.getResource("/").toString();
+        logger.info("!!!!!!!!!!!!!!!!!!!!!! path is {}", prefix);
         int index = prefix.indexOf("target/classes/");
         prefix = prefix.substring(0, index);
         String path = prefix + "src/main/resources/static/media/";
         registry.addResourceHandler("/media/**")
                 .addResourceLocations(path);
     }
+
 }
